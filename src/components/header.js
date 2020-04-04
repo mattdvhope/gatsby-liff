a6d8a9fe95efeb2e61b6ce9f7302a0998a73e750
mattdvhope <mattagape@me.com>
2020-04-05 06:01:26 +0700

    Styling improvements

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `rgb(239, 239, 239)`,
       marginBottom: `1.45rem`,
     }}
   >
@@ -20,8 +20,9 @@ const Header = ({ siteTitle }) => (
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `rgb(45, 49, 121)`,
             textDecoration: `none`,
+            fontFamily: `Athiti`
           }}
         >
           {siteTitle}
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,19 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi City English Project</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1 style={{ color: `rgb(139, 195, 74)`, fontFamily: `Athiti` }}>
+      เรียนภาษาอังกฤษออนไลน์กับชาวอเมริกัน
+    </h1>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
 
     <a href="https://cep-liff.herokuapp.com/">LOGIN</a>
-{/* <Link to="/page-2/">Go to page 2</Link>  */}
+    <p>------------------------</p>
+    <Link to="/page-2/">Go to page 2</Link>
+    <p>------------------------</p>
+    <link href="https://fonts.googleapis.com/css?family=Athiti|Chonburi|Kanit|Maitree|Prompt|Sriracha|Taviraj|Trirong|Josefin+Sans" rel="stylesheet" />
+
   </Layout>
 )
 
