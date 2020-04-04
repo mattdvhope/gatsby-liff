83e66d2a31f8f97eff2ba2811e96295d982dbb31
mattdvhope <mattagape@me.com>
2020-04-04 19:24:20 +0700

    Changed link for login

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,9 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+
+    <a href="https://cep-liff.herokuapp.com/">LOGIN</a>
+{/* <Link to="/page-2/">Go to page 2</Link>  */}
   </Layout>
 )
 
