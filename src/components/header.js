b8324308e119b32b4c201303b0103d1f1eab6816
mattdvhope <mattagape@me.com>
2020-04-05 06:38:11 +0700

    Image additions

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Image1 from "../components/image1"
 
 const Header = ({ siteTitle }) => (
   <header
@@ -16,7 +17,12 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
+
       <h1 style={{ margin: 0 }}>
+        <div style={{ maxWidth: `50px`, marginBottom: `1.2rem` }}>
+          <Image1 />
+        </div>
+
         <Link
           to="/"
           style={{
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,14 +11,13 @@ const IndexPage = () => (
     <h1 style={{ color: `rgb(139, 195, 74)`, fontFamily: `Athiti` }}>
       เรียนภาษาอังกฤษออนไลน์ฟรีกับชาวอเมริกัน
     </h1>
+    <a href="https://cep-liff.herokuapp.com/">เข้าสู่ระบบโดยใช้ LINE</a>
+    <p>------------------------</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
 
-    <a href="https://cep-liff.herokuapp.com/">LOGIN</a>
-    <p>------------------------</p>
     <Link to="/page-2/">Go to page 2</Link>
-    <p>------------------------</p>
     <link href="https://fonts.googleapis.com/css?family=Athiti|Chonburi|Kanit|Maitree|Prompt|Sriracha|Taviraj|Trirong|Josefin+Sans" rel="stylesheet" />
 
   </Layout>
