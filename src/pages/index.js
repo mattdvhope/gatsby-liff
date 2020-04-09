b54881afd5af30ca20441c522eaa890789e31896
mattdvhope <mattagape@me.com>
2020-04-09 17:26:22 +0700

    Changed expiration

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,6 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 import Login from "../components/Login"
 
-
-// let redirection_loc;
-// let redirection_loc_prod = `https%3A%2F%2Fquizzical-payne-4097ad.netlify.com%2Fapp%2Fprofile`;
-// let redirection_loc_deve = process.env.REDIRECT_URI;
-// redirection_loc = redirection_loc_deve ? redirection_loc_deve : redirection_loc_prod;
-
-// console.log("uri", process.env.REDIRECT_URI);
-// console.log("uri", redirection_loc);
-
 let madeState = makeState(10);
 
 const IndexPage = () => (
@@ -23,7 +14,7 @@ const IndexPage = () => (
     </h1>
     <h2>
       <a
-        href={`https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1654045933&redirect_uri=${process.env.GATSBY_API_URL}&state=${madeState}&scope=profile%20openid&max_age=30&ui_locales=th&bot_prompt=aggressive`}
+        href={`https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1654045933&redirect_uri=${process.env.GATSBY_API_URL}&state=${madeState}&scope=profile%20openid&max_age=2592000&ui_locales=th&bot_prompt=aggressive`}
         style={{
           color: `rgb(45, 49, 121)`,
           fontFamily: `Athiti`,
